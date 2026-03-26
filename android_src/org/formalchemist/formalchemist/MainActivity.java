0eb8d39eae9b0770b63e966b48112fc97212e512
rmtaa0916 <154336288+rmtaa0916@users.noreply.github.com>
2026-03-26 03:23:58 -0700

    Add files via upload

--- android_src/org/formalchemist/formalchemist/MainActivity.java
+++ android_src/org/formalchemist/formalchemist/MainActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.ImageView;
 
 import org.kivy.android.PythonActivity;
 
 public class MainActivity extends PythonActivity {
     private static final String TAG = "FormAlchemistMainActivity";
+    private static final long LOADING_FADE_MS = 260L;
+
+    private boolean loadingFadeStarted = false;
 
     public interface NewIntentListener extends PythonActivity.NewIntentListener {}
     public interface ActivityResultListener extends PythonActivity.ActivityResultListener {}
@@ -49,7 +53,77 @@ public class MainActivity extends PythonActivity {
             view.setImageDrawable(new ColorDrawable(Color.BLACK));
         }
 
+        view.setAlpha(1f);
         mImageView = view;
+        loadingFadeStarted = false;
         return view;
+    }
+
+    @Override
+    protected void removeLoadingScreen() {
+        if (mImageView == null) {
+            super.removeLoadingScreen();
+            return;
+        }
+        if (loadingFadeStarted) {
+            Log.v(TAG, "removeLoadingScreen called again while fade is in progress");
+            return;
+        }
+
+        final ImageView target = mImageView;
+        loadingFadeStarted = true;
+        Log.v(TAG, "Fading out native loading screen");
+
+        target.post(new Runnable() {
+            @Override
+            public void run() {
+                if (target != mImageView) {
+                    loadingFadeStarted = false;
+                    return;
+                }
+
+                ViewParent parent = target.getParent();
+                if (!(parent instanceof ViewGroup)) {
+                    Log.v(TAG, "Loading screen parent missing, clearing image view reference");
+                    clearTrackedImageView(target);
+                    return;
+                }
+
+                target.animate()
+                        .alpha(0f)
+                        .setDuration(LOADING_FADE_MS)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                Log.v(TAG, "Native loading screen fade finished");
+                                removeTrackedImageView(target);
+                            }
+                        })
+                        .start();
+            }
+        });
+    }
+
+    private void removeTrackedImageView(ImageView target) {
+        if (target == null) {
+            loadingFadeStarted = false;
+            return;
+        }
+
+        target.animate().cancel();
+        ViewParent parent = target.getParent();
+        if (parent instanceof ViewGroup) {
+            ((ViewGroup) parent).removeView(target);
+        }
+        clearTrackedImageView(target);
+    }
+
+    private void clearTrackedImageView(ImageView target) {
+        target.setImageDrawable(null);
+        target.setAlpha(1f);
+        if (mImageView == target) {
+            mImageView = null;
+        }
+        loadingFadeStarted = false;
     }
 }
